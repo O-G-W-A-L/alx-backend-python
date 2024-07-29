29cf11bf52d1c731791153c3b17f7b40cc6c3854
O-G-W-A-L <ogwalja04@gmail.com>
2024-07-29 06:18:29 +0000

    fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -64,8 +64,8 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once()
 
     @parameterized.expand([
-        ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
-        ({'license': {'key': "bsl-1.0"}}, "bsd-3-clause", False),
+        ({'license': {'key': "my_license"}}, "my_license", True),
+        ({'license': {'key': "other_license"}}, "my_license", False),
     ])
     def test_has_license(
         self, repo: Dict, license_key: str, expected: bool
