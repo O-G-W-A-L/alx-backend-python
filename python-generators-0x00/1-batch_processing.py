5b4ee58fed9195b23b2e9e836d74277dd683e828
O-G-W-A-L <ogwal04@gmail.com>
2025-05-16 18:19:56 +0300

    Batch processing Large Data

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,24 +2,17 @@ import mysql.connector
 from mysql.connector import Error
 
 def stream_users_in_batches(batch_size):
-    """Generator function to stream rows from the user_data table in batches."""
+    """Generator function to stream rows from the user_data table in batches"""
     connection = None
     try:
-        # Connect to the MySQL database
         connection = mysql.connector.connect(
             host='localhost',
             user='hunter',
             password='pass',
             database='ALX_prodev'
         )
-
-        # Create a cursor object to interact with the database
         cursor = connection.cursor(dictionary=True)
-
-        # Execute the SQL query to select all rows from user_data
         cursor.execute("SELECT * FROM user_data")
-
-        # Fetch and yield rows in batches
         while True:
             batch = cursor.fetchmany(batch_size)
             if not batch:
@@ -29,17 +22,14 @@ def stream_users_in_batches(batch_size):
     except Error as e:
         print(f"Error: {e}")
     finally:
-        # Close the connection if it was established
         if connection and not connection.is_closed():
             connection.close()
+    return
 
 def batch_processing(batch_size):
-    """Generator function to process each batch and filter users over the age of 25."""
-    # Iterate over each batch fetched from the database
+    """Generator function to process each batch and filter users over the age of 25"""
     for batch in stream_users_in_batches(batch_size):
-        # Iterate over each user in the batch
         for user in batch:
-            # Check if the user's age is over 25
             if user['age'] > 25:
-                # Yield the user if they meet the condition
                 yield user
+    return
