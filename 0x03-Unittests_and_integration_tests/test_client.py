506152ed5cdcd921d97792d1929b0f6584f3d96b
O-G-W-A-L <ogwalja04@gmail.com>
2025-05-29 19:49:22 +0000

    test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -99,22 +99,23 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             'https://api.github.com/orgs/google': cls.org_payload,
             'https://api.github.com/orgs/google/repos': cls.repos_payload,
         }
-        cls.get_patcher = patch(
-            "requests.get", side_effect=cls.mocked_requests_get
+
+    def setUp(self) -> None:
+        """Set up instance fixtures."""
+        self.get_patcher = patch(
+            "requests.get", side_effect=self.mocked_requests_get
         )
-        cls.mock_get = cls.get_patcher.start()
+        self.mock_get = self.get_patcher.start()
 
-    @classmethod
-    def mocked_requests_get(cls, url: str):
-        """Mock the requests.get method to return the appropriate payload."""
-        if url in cls.route_payload:
-            return Mock(**{'json.return_value': cls.route_payload[url]})
+    def tearDown(self) -> None:
+        """Tear down instance fixtures."""
+        self.get_patcher.stop()
+
+    def mocked_requests_get(self, url: str):
+        """Mock requests.get to return payloads."""
+        if url in self.route_payload:
+            return Mock(**{'json.return_value': self.route_payload[url]})
         raise HTTPError()
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        """Remove class fixtures after running all tests."""
-        cls.get_patcher.stop()
 
     def test_public_repos(self) -> None:
         """Test the public_repos method."""
@@ -127,4 +128,3 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(
             client.public_repos(license="apache-2.0"), self.apache2_repos
         )
-
