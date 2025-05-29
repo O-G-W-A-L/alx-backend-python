e10dc85561d352726519b5822ebfd6880f92f8d5
O-G-W-A-L <ogwalja04@gmail.com>
2025-05-29 18:38:08 +0000

    test edit

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -69,6 +69,7 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     def test_has_license(self,
                      repo: Dict, license_key: str, expected: bool) -> None:
+
         """Test the has_license method."""
         client = GithubOrgClient("google")
         self.assertEqual(client.has_license(repo, license_key), expected)
