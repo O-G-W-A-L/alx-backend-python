82c14ce142b3554a174c07c23d215e56da8a9a2e
O-G-W-A-L <ogwal04@gmail.com>
2025-06-12 22:02:01 +0300

    implement basic view cache

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from .models import Message
+from django.views.decorators.cache import cache_page
 
 @login_required
 def delete_user(request):
@@ -17,6 +18,7 @@ def delete_user(request):
         'user': request.user
     })
 
+@cache_page(60)
 def conversation_view(request, conversation_user_id):
     user = request.user
     # Fetch root messages for the conversation with the specified user
@@ -45,4 +47,3 @@ def conversation_view(request, conversation_user_id):
 def unread_messages_view(request):
     unread_messages = Message.unread.unread_for_user(request.user).only('id', 'sender', 'content', 'timestamp')
     return render(request, 'messaging/unread_messages.html', {'unread_messages': unread_messages})
-
